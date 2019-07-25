3c5adc60123f9d302cdb2a7b7eccd295bc9888c3
PARK Yong Seo <j@saro.me>
2019-07-25 23:45:25 +0900

    app setting

--- admin-web-project/src/main/java/web/Application.java
+++ admin-web-project/src/main/java/web/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@ComponentScan({"web"})
+@ComponentScan({"web", "web.*"})
 public class Application {
 
     public static void main(String[] args) {
